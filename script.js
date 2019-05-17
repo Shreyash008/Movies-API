5f326c1f7b17abd9861bb574cc34dcebfda15d30
Shreyash008 <41724191+Shreyash008@users.noreply.github.com>
2019-05-17 16:17:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const dev = document.getElementById('main');
-const logo = document.createElement('img');
-logo.src = 'logo.png';
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
-dev.appendChild(logo);
-de.appendChild(container);
-var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-request.onload = function () {
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+const image = document.createElement('img');
+image.src = 'logo.png';
+const data = document.createElement('div');
+data.setAttribute('class', 'data');
+dev.appendChild(image);
+dev.appendChild(data);
+var R1 = new XMLHttpRequest();
+R1.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+R1.onload = function () {
+  var ele = JSON.parse(this.response);
+  if (R1.status >= 200 && R1.status < 400) {
+    ele.forEach(movie => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
       const h1 = document.createElement('h1');
       h1.textContent = movie.title;
-      const p = document.createElement('p');
+      const info = document.createElement('info');
       movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
-      container.appendChild(card);
+      info.textContent = `${movie.description}...`;
+      data.appendChild(card);
       card.appendChild(h1);
-      card.appendChild(p);
-    });
+      card.appendChild(info);
+    })
   } 
 }
-request.send();
+R1.send();
