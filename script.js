8da6a5794b7f61ae2547455ae45950be98a2f3f3
Shreyash008 <41724191+Shreyash008@users.noreply.github.com>
2019-05-17 13:27:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ const logo = document.createElement('img');
 logo.src = 'logo.png';
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
-
 app.appendChild(logo);
 app.appendChild(container);
-
 var request = new XMLHttpRequest();
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 request.onload = function () {
@@ -15,14 +13,11 @@ request.onload = function () {
     data.forEach(movie => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
-
       const h1 = document.createElement('h1');
       h1.textContent = movie.title;
-
       const p = document.createElement('p');
       movie.description = movie.description.substring(0, 300);
       p.textContent = `${movie.description}...`;
-
       container.appendChild(card);
       card.appendChild(h1);
       card.appendChild(p);
@@ -33,5 +28,4 @@ request.onload = function () {
     app.appendChild(errorMessage);
   }
 }
-
 request.send();
